4b9548273076aa162b546be75ea8cd75f9d3951f
Sourav Mazumdar <sourav.mozu@gmail.com>
2024-07-23 13:55:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //your JS code here. If required.
 box=document.getElementById("fname")
-name1=box.value
-name2 = name1.toUpperCase()
-box.value=name2
+box.addEventListener("blur",func1)
+function func1(){
+	name1=box.value
+	name2 = name1.toUpperCase()
+	box.value=name2
+}
