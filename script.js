83d2a01d88178051d73159e7d1c2861c7613b80c
Sourav Mazumdar <sourav.mozu@gmail.com>
2024-07-23 13:20:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 box=document.getElementById("fname")
-name=box.innerText
-name = name.toUpperCase()
-box.innerText=name
+name1=box.value
+name2 = name1.toUpperCase()
+box.value=name2
